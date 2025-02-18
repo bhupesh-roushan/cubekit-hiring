02e92f3afe8a248cf5191f086824fcd0b0e444be
bhupesh-roushan <roushan.bhupesh@gmail.com>
2025-02-18 12:17:06 +0530

    added supabase

--- src/pages/PostJobs.jsx
+++ src/pages/PostJobs.jsx
@@ -90,7 +90,7 @@ const PostJobs = () => {
   return (
     <div className="mb-5">
       <h1 className="gradient-title font-extrabold text-5xl sm:text-7xl text-center pb-8">
-        Post a Job
+        Post Job
       </h1>
 
       <form
